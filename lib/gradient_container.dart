b9571af8737c0fd04b3b62bd27cfca4c685ae075
Daniel Julian Dela Cruz Remoreras <djdremoreras@JF612-M2012.local>
2025-08-04 12:17:53 +0800

    added magicballwidget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:magic_ball/magic_ball_widget.dart';
 import 'package:magic_ball/style_text.dart';
 
 const Alignment alignmentTopLeft = Alignment.topLeft;
 const Alignment alignmentBottomRight = Alignment.bottomRight;
+List<String> listOfWords = ['hello', 'world'];
+List<int> listOfNumbers = [1, 2, 3, 4, 5];
 
 //const topLeft = Alignment.topLeft;
 //const greeting = 'Hello World!';
 //final bottomRight = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
+  //var ballImage = "assets/images/ball_images/1.png";
+
   const GradientContainer({super.key});
 
   @override
@@ -17,15 +22,15 @@ class GradientContainer extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            const Color.fromARGB(255, 136, 223, 134),
-            const Color.fromARGB(255, 136, 118, 25),
+            const Color.fromARGB(255, 202, 145, 239),
+            const Color.fromARGB(255, 73, 8, 92),
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: Center(
-        child: Image.asset("assets/images/ball_images/1.png"),
+        child: MagicBallWidget(),
       ),
     );
   }
---
+++ lib/magic_ball_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:magic_ball/gradient_container.dart';
+import 'package:magic_ball/style_text.dart';
+
+class MagicBallWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MagicBallState();
+  }
+}
+
+class _MagicBallState extends State<MagicBallWidget> {
+  String ballImage = "assets/images/ball_images/1.png";
+  void shake() {
+    setState(() {
+      int randomIndex =
+          (DateTime.now().millisecondsSinceEpoch / 1000).round() % 5 + 1;
+      ballImage = 'assets/images/ball_images/$randomIndex.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const StyleText("Ask a question..."),
+        const SizedBox(height: 50),
+        Image.asset(ballImage, width: 300),
+        const SizedBox(
+          height: 30,
+        ),
+        TextButton(
+          onPressed: shake,
+          child: Text(
+            "Shake",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 2,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
